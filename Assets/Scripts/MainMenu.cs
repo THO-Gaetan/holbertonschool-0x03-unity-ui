5a38727d5bce10095965823efff93a55be0a8a8c
THO-Gaetan <9771@holbertonschool.com>
2026-05-15 15:00:50 +0200

    adding the Colorblind options to the menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,6 +22,16 @@ public class MainMenu : MonoBehaviour
 
     public void PlayMaze()
     {
+        if (colorblindMode.isOn == true)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+        else
+        {
+            trapMat.color = Color.red;
+            goalMat.color = Color.green;
+        }
         SceneManager.LoadScene(0);
     }
 
